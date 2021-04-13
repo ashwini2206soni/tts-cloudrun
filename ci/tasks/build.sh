f86b6fe3987653c0de39ee90bbf59ff8106b4720
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-04-13 02:46:55 -0400

    test

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-
+cd text-speech/
 echo "${TF_PROVIDER_GCP_CREDENTIALS}" > credentials.json
 gcloud auth activate-service-account ashwini-consulting-sa@ashwini-consulting.iam.gserviceaccount.com --key-file=credentials.json --project='ashwini-consulting'
 
