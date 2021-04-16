8d413e7c81705253216fd74e2c81883f625513ed
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-04-16 02:24:10 -0400

    changed version of docker image

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -5,4 +5,4 @@ cd text-speech/
 echo "${TF_PROVIDER_GCP_CREDENTIALS}" > credentials.json
 gcloud auth activate-service-account ashwini-consulting-sa@ashwini-consulting.iam.gserviceaccount.com --key-file=credentials.json --project='ashwini-consulting'
 
-gcloud builds submit --tag gcr.io/ashwini-consulting/front:v2
+gcloud builds submit --tag gcr.io/ashwini-consulting/front:v3
